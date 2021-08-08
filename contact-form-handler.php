0e994157fce2faf97ce6b7d18453e09bdfe46c79
Anu Goyal <a.goyal@ltp.nl>
2021-08-08 22:44:17 +0200

    contact form changes

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,15 +1,22 @@
 <?php
-  $name = $_POST['name'];
-  $visitorEmail = $_POST['email'];
-  $message = $_POST['message'];
-  $subject = $_POST['subject'];
+  $name = $_GET['name'];
+  $visitorEmail = $_GET['email'];
+  $message = $_GET['message'];
+  $subject = $_GET['subject'];
 
-  $emailBody = "User Name: $name. \n".
-               "User Email: $visitorEmail. \n".
-               "User Message: $message. \n".
-               "User Subject: $subject. \n";
+  $emailBody = "User Name: $name \n".
+               "User Email: $visitorEmail \n".
+               "User Message: $message \n".
+               "User Subject: $subject \n";
   $to = "goyalanu6@gmail.com";
-  $headers = "Reply-To: $visitorEmail \r\n"
-  mail($to,$subject,$emailBody,$headers);
+  $headers = "Reply-To: $visitorEmail" . "\n";
+  $headers .= 'Content-Type: text/plain; charset="iso-8859-1"' . "\n";
+  $headers .= 'Content-Transfer-Encoding: 8bit';
+
+if (mail($to,$subject,$emailBody,$headers)) {
+  echo "email sent!";
+} else {
+   echo "error";
+}
   header("Location: index.html");
 ?>
